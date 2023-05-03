1eb434f56dcff8a08dcd4e4ef1f33df4d27f4796
Kyom <momosan0304@gmail.com>
2023-05-03 18:31:43 +0900

    htaccess name revised

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // // リダイレクト
     // location.replace(newUrl);
-}
+    // }
 
     /* =================================================== */
     // スクロールのon,off切り替えの関数
