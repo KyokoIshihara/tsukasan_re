24fee71c6bc2f215732fdce2cc01f7e4d7ee7862
Kyom <momosan0304@gmail.com>
2023-05-03 18:24:16 +0900

    htaccess revised

--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
     
+    // /* =================================================== */
+    // // http→httpsへのリダイレクト
+    // /* =================================================== */
+    // var url = location.href
+
+    // if(url.indexOf('http:') >= 0) { // 判定方法の修正
+    // // url文字列の書き換え
+    // var newUrl = url.replace('http:', 'https:'); // 書き換え方法の修正
+
+    // // リダイレクト
+    // location.replace(newUrl);
+}
 
     /* =================================================== */
     // スクロールのon,off切り替えの関数
